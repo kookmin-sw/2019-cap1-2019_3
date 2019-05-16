d65bdd6d964c7344ad08353bb2a0ab1fd75b3b9b
Son Tae Seon <escs34@gmail.com>
2019-05-17 08:39:51 +0900

    Add new logic for timeline in commander
    time_line need data, img, and also time
    so make new function for support it

--- src/ohtube/yougam/code/VideoModule/Commander.py
+++ src/ohtube/yougam/code/VideoModule/Commander.py
@@ -107,7 +107,6 @@ class Commander:
                  pred_emotion_average_list = [np.average(each) for each in pred_array]
                  pred_array = np.transpose(pred_emotion_average_list)
 
-                 #print(pred_array)
                  if len(pred_array) != 0:
                      emotion_list.append(pred_array)
             
@@ -117,7 +116,7 @@ class Commander:
         emotion_array = np.transpose(emotion_array)
 
         emotion_average_list = [np.average(each) for each in emotion_array]
-        #print( len(emotion_average_list) )
+
         EMOTIONS = ["angry","disgust","scared", "happy", "sad", "surprised","neutral"]
         emotion_dict_list = []#dict()
         emotion_dict = dict()
@@ -130,7 +129,83 @@ class Commander:
         return dumped
 
 
+    def for_youtube_video_TimeLine(self, use_i_th_frame, video_url):
+        #download
+        serial = video_url.split('v=')[1]
+        download_path = './videos'
+        print('download path : ', download_path)
 
+        video_name = downloadYouTube_if_not_exist(video_url, download_path, serial)
+        print(video_name)
+
+        #registing
+        print("start registing")
+        self.imgLoader.registVideo(video_name)
+
+        #process (use loop)
+        emotion_list = []
+        face_list = []
+        i_list=[]
+        i=0
+        print("start extracting")
+        faces = []
+        while(self.imgLoader.isOpened()):
+            i+=1
+
+            if i%500 == 0:
+                print(i,'th frame is proceed.')
+
+            img = self.imgLoader.getThisFrame()
+
+            if img is None:
+                print('img == None')
+                break
+
+            if i%use_i_th_frame==0:#per i th frame
+                 preds = [[0, 0, 0, 0, 0 ,0 ,0]]
+                 preds, faces = self.oracle.predict_and_return_others(img)
+
+                 pred_array = np.array(preds)
+                 pred_array = np.transpose(pred_array)
+                 pred_emotion_average_list = [np.average(each) for each in pred_array]
+                 pred_array = np.transpose(pred_emotion_average_list)
+
+                 if len(pred_array) != 0:
+                     emotion_list.append(pred_array)
+                     for (fX,fY,fW,fH) in faces:#add face picture
+                         roi = img[fY:fY + fH, fX:fX + fW]#cut face image
+                         break
+                     face_list.append(roi)
+
+                     i_list.append(i)
+
+        #print('start making json')
+        emotion_array = np.array(emotion_list)
+
+        emotion_array = np.transpose(emotion_array)
+
+        emotion_average_list = [np.average(each) for each in emotion_array]
+
+        EMOTIONS = ["angry","disgust","scared", "happy", "sad", "surprised","neutral"]
+        emotion_dict_list = []#dict()
+        emotion_dict = dict()
+        for j in range(len(emotion_average_list)):#(7):
+            emotion_dict[ EMOTIONS[j] ] = emotion_average_list[j]
+            emotion_dict_list.append( { "label":EMOTIONS[j], "value":emotion_average_list[j] } )
+
+        dumped = json.dumps(str(emotion_dict_list))[1:-1]
+        dumped = dumped.replace("'", '"')
+
+        max_emotion_list = [ EMOTIONS[np.argmax(each)] for each in np.transpose(emotion_array)]
+
+        '''#use this code if you want to see the faces
+        for each in face_list:
+            cv2.imshow("image", each)
+            cv2.waitKey(300)#pause for 0.010 second 1000:1s = 1000/30=0.33 : 1f
+        cv2.destroyAllWindows()
+        '''
+
+        return dumped, max_emotion_list, i_list, face_list
 
 
 
